c9233c3e371f728cdae4c37cbf0b8018b04d4563
RAMAPRABHUPERIYASAMY <103096309+RAMAPRABHUPERIYASAMY@users.noreply.github.com>
2022-04-26 10:26:11 +0530

    Activities completed

---
+++ Selenium/Activities/CSVReader.java
@@ -0,0 +1,17 @@
+package Activities;
+
+import java.io.FileReader;
+import java.util.List;
+
+public class CSVReader {
+    public CSVReader(FileReader fileReader) {
+    }
+
+    public List<String[]> readAll() {
+
+        return null;
+    }
+        public void close()
+        {
+        }
+    }
