18b7f3150cbab948fe98f4effd95bdcd5d90e2f3
RAMAPRABHUPERIYASAMY <103096309+RAMAPRABHUPERIYASAMY@users.noreply.github.com>
2022-04-26 10:30:01 +0530

    Activities completed

---
+++ TestNG/Activities/Activities_1.java
@@ -0,0 +1,19 @@
+package Activities;
+
+public class Activities_1
+{
+    @Test(priority = 0)
+public void One() {
+    System.out.println("This is the Test Case number One");
+}
+
+    @Test(priority = 1)
+    public void Two() {
+        System.out.println("This is the Test Case number Two");
+    }
+
+    @Test(priority = 2)
+    public void Three() {
+        System.out.println("This is the Test Case number Three");
+    }
+}
