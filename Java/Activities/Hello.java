4298c3cf15ccebe99933103a15bee0911ea2ae56
RAMAPRABHUPERIYASAMY <103096309+RAMAPRABHUPERIYASAMY@users.noreply.github.com>
2022-04-26 10:24:44 +0530

    Activities Completed

---
+++ Java/Activities/Hello.java
@@ -0,0 +1,12 @@
+package examples;
+
+public class Hello {
+    public static void main(String[] args) {
+        String welcome = ("Indian");
+        System.out.println(welcome);
+
+        Boolean Value = true;
+        System.out.println(Value);
+    }
+
+}
