a8291105ae2a0a1577df3fc14ff795d0e5e56431
RAMAPRABHUPERIYASAMY <103096309+RAMAPRABHUPERIYASAMY@users.noreply.github.com>
2022-04-28 12:58:05 +0530

    Refreshed

---
+++ TestNG/Activities/Activity4.java
@@ -0,0 +1,13 @@
+package Activities;
+
+public class Activity4<classes, xml> {
+     <?xml version="1.0" encoding="UTF-8"?>
+<suite name="ExampleSuite" >
+    <test name="SuiteTests" >
+        <classes>
+            <class name="suiteExample.DemoOne"/>
+            <class name="suiteExample.DemoTwo"/>
+        </classes>
+    </test>
+</suite>
+}
